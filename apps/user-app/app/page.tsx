cfefaa1e96b9b686a0a108d11f1a3217ee98b725
Tushar <tusharschdeva16@gmail.com>
2025-08-26 01:53:04 +0530

    Test CI/CD pipeline

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -10,6 +10,7 @@ import { authOptions } from "./lib/auth";
 export default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
+    
     redirect('/dashboard')
   } else {
     redirect('/api/auth/signin')
